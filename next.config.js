f58beb7ab95de7ff8eedf6b1d8359f68aac811f9
Collins Ezedike-egwom <ezedikecollins@gmail.com>
2023-05-24 08:59:22 +0100

    Configure the next.config file to run mongoose

--- next.config.js
+++ next.config.js
@@ -1,4 +1,15 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {}
+const nextConfig = {
+  experimental: {
+    serverComponentsExternalPackages: ["mongoose"],
+  },
+  webpack: (config) => {
+    config.experiments = {
+      topLevelAwait: true,
+      layers: true,
+    };
+    return config;
+  },
+};
 
-module.exports = nextConfig
+module.exports = nextConfig;
