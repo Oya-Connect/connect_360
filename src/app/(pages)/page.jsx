019a9429c4000a77d367851d91c60f6a75f7217b
armyofdeux <103246856+Emmanuetoks@users.noreply.github.com>
2023-06-20 13:03:37 +0100

    chore: Make the slides Array a node list using Ref (#9)

--- src/app/(pages)/page.jsx
+++ src/app/(pages)/page.jsx
@@ -1,27 +1,34 @@
 "use client";
+import { useRef } from "react";
 import Carousel from "./components/home/Carousel";
 
 const Home = () => {
+  const slides = useRef([]);
+  
+  // Function to add the slide Nodes to a ref
+  const addSlide = (el) => slides.current.push(el)
+
   return (
     <>
     <header className="header--home w-full h-96 relative">
 
       {/* Example of how to use the carousel */}
-      <Carousel>
-        <div className="carousel__slide bg-slate-900 z-10">
+      <Carousel slides={slides.current}>
+        <div ref={addSlide} className="carousel__slide bg-slate-900 z-10">
           dummy slide
         </div>
-        <div className="carousel__slide bg-orange-900">
+        <div ref={addSlide} className="carousel__slide bg-orange-900">
           dummy slide
         </div>
-        <div className="carousel__slide bg-red-900">
+        <div ref={addSlide} className="carousel__slide bg-red-900">
           dummy slide
         </div>
-        <div className="carousel__slide bg-blue-900">
+        <div  ref={addSlide} className="carousel__slide bg-blue-900">
           dummy slide
         </div>
       </Carousel>
     </header>
+    
     <main className="main--home">
       
     </main>
