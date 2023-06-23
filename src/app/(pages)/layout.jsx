3687a68c9c36a8335176aa50ca52ea42a15bb1c9
Anuoluwa Adebayo <adebayo.anuoluwa02@gmail.com>
2023-06-23 02:57:27 +0100

    Footer CSS and HTML creation

--- src/app/(pages)/layout.jsx
+++ src/app/(pages)/layout.jsx
@@ -5,7 +5,56 @@ const AppLayout = ({ children }) => {
     <div className="app">
       <NavBar />
       {children}
-      <footer className="app__footer"></footer>
+      <footer className="footer">
+        <div className="container">
+          <div className="cyp-row">
+            <div className="col-lg-4 col-md-12">
+              <div className="footer-brand">
+                {/* Reposition the image file if possible  */}
+                <img
+                  src="../public/images/logo.png"
+                  alt=""
+                />
+                <div className="footer-social">
+                  {/* Import the fontawesome CSS module to enable these icons  */}
+                  <a href="#">
+                    <i className="fab fa-instagram"></i>
+                  </a>
+                  <a href="#">
+                    <i className="fab fa-whatsapp"></i>
+                  </a>
+                  <a href="#">
+                    <i className="fab fa-twitter"></i>
+                  </a>
+                  <a href="#">
+                    <i className="fab fa-facebook"></i>
+                  </a>
+                </div>
+              </div>
+            </div>
+            <div className="col-lg-4 col-md-12">
+              <ul className="footer-list">
+                <div className="footer-list-header">Other Links</div>
+                <li className="footer-item">
+                  <a href="#">Admin sign in</a>
+                </li>
+                <li className="footer-item">
+                  <a href="#">About us</a>
+                </li>
+                <li className="footer-item">
+                  <a href="#">Contact us</a>
+                </li>
+              </ul>
+            </div>
+          </div>
+          <div clasName="cyp-row">
+            <div className="footer-credits">
+              &copy;<span id="dateYear">2023</span>{" "}
+              <a href="#">www.connect.ng.</a> All Rights Reserved.
+            </div>
+          </div>
+        </div>
+      </footer>
     </div>
   );
 };
