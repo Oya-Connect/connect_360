9c1b7b21df60589d6b9ba586b16988ba31eab971
armyofdeux <103246856+Emmanuetoks@users.noreply.github.com>
2023-06-23 10:44:18 +0100

    Frontend: Footer Created (#14)
    * chore: Make the slides Array a node list using Ref
    
    * chore: Fix potential bug (#10)
    
    * Unnamed add footer (#13)
    
    * feat: delete readme.MD content for update
    
    * Footer CSS and HTML creation
    
    * Merge from Tochukwu (#12)
    
    * component: finish carousel logic
    Finished the carousel loguc but i havent made it responsive yet.
    
    * component: Finish carousel
    Finished the reusable carousel component and made it responsive
    
    * chore: made commments ont how the carousel works
    
    * chore: Make the slides Array a node list using Ref
    
    * chore: Make the slides Array a node list using Ref (#9)
    
    * chore: Fix potential bug
    
    * chore: Fix potential bug (#10)
    
    * component: create starting files for auth page
    Just added some starting files and the basic file structure for the authpage
    
    * chore: Tidy up scattered files and include footer
    Incorporated css modules and made a footer component because it was too big.
    
    ---------
    
    Co-authored-by: Anuoluwa Adebayo <adebayo.anuoluwa02@gmail.com>
    
    ---------
    
    Co-authored-by: Anuoluwa Adebayo <adebayo.anuoluwa02@gmail.com>

--- src/app/(pages)/layout.jsx
+++ src/app/(pages)/layout.jsx
@@ -1,11 +1,12 @@
 import NavBar from "@/app/(pages)/components/global/NavBar";
+import Footer from "./components/global/Footer";
 
 const AppLayout = ({ children }) => {
   return (
     <div className="app">
       <NavBar />
       {children}
-      <footer className="app__footer w-f"></footer>
+      <Footer />
     </div>
   );
 };
